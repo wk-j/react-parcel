4893a681ba765901a8c839a640439536fa093f3f
wk-j <somnuk.wk@gmail.com>
2018-01-30 22:54:42 +0700

    Update README

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-## Bundle React App with Parcel
+## React Parcel
+
+Bundle React app with Parcel
 
 ```bash
 parcel src/Index.html
--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const logo = require("./images/parcel.png")
 export class App extends React.Component<{}, {}> {
     render() {
         return (
-            <h1>
+            <h1 style={{ color: "blue" }}>
                 <img width="500" src={logo} />
                 <h1>Hello, world!</h1>
             </h1>
