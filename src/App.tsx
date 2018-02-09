df96754dfa09716bb30268f6d8f6e97c18c93724
Somnuk Wongkhan <somnuk.wk@gmail.com>
2018-02-09 18:37:58 +0700

    :zap: Added state

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,47 @@ import * as ReactDOM from "react-dom"
 
 const logo = require("./images/parcel.png")
 
-export class App extends React.Component<{}, {}> {
+type State = {
+    count: number
+}
+
+export class App extends React.Component<{}, State> {
+    constructor(props) {
+        super(props)
+        this.state = { count: 0 }
+    }
+    add = () => {
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
     render() {
         return (
-            <h1 style={{ color: "blue" }}>
+            <div>
                 <img width="500" src={logo} />
-                <h1>Hello, world!</h1>
-            </h1>
+                <h1 onClick={this.add}>Hello, world! {this.state.count}</h1>
+            </div>
         );
     }
 }
 
-var app = document.getElementById("app")
-ReactDOM.render(<App />, app)
+let render = App => {
+    var app = document.getElementById("app")
+    ReactDOM.render(<App />, app);
+}
+
+render(App);
+
+declare var module: any;
+
+if (module.hot) {
+    console.log("hot")
+    module.hot.dispose(function () {
+        console.log("dispose")
+        // module is about to be replaced
+    });
+
+    module.hot.accept(() => {
+        // module or one of its dependencies was just updated
+    });
+}
